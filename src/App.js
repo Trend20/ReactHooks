4efb2c0e011a650c91ade999b2f5ddbd827aa774
DevEnock <enockomondi305@gmail.com>
2022-03-08 23:38:45 +0300

    counter example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const changeCounter = () =>{
+    setCounter(counter + 1)
+  }
   return (
     <div className="App">
       <h1>React Hooks</h1>
+      <p>{counter}</p>
+      <button onClick={changeCounter}>Change Counter</button>
     </div>
   );
 }
